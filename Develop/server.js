d4ad8f27a66af60d43c352fcf19c4f23632a9f6b
JayRodButray <jayrodbutray@gmail.com>
2023-07-26 12:49:36 -0700

    first successful api call

---
+++ Develop/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const path= require('path');
+const noteData = require('./db/db.json');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/', (req, res) => res.send("Click button to start adding notes"));
+
+app.get('/api/notes', (req, res) => res.json(noteData));
+
+app.post('/api/notes', (req, res) => {
+
+    res.json(`${req.method} request received to add a note`);
+  
+
+    console.info(`${req.method} request received to add a note`);
+  });
+
+app.listen(3001, () => {
+    console.log('server is running');
+});
